e1229d08e81e72b7996fbcc4bfaed525d83d3a6e
daniel-c-souza <119331502+daniel-c-souza@users.noreply.github.com>
2024-09-12 16:04:47 -0300

    setima tentativa de gerar docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,12 @@ release = '12/09/2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
-
+extensions = [
+   'sphinx.ext.duration',
+   'sphinx.ext.doctest',
+   'sphinx.ext.autodoc',
+   'sphinx.ext.autosummary',
+]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
